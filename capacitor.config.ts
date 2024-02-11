2bc41f045e260a244123b2a7d8361b9a7d45e717
Lars Behrmann <lars.behrmann@catchium-gmbh.de>
2024-02-11 21:50:05 +0100

    Set app license to Apache 2 license

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,3 +1,26 @@
+/*
+ * File: capacitor.config.ts
+ * Project: blueprint
+ * File Created: Sunday, 4th February 2024
+ * Author: Lars Behrmann (larsbehrmann@behrmann-it.de)
+ * -----
+ * Last Modified: Sunday, 11th February 2024
+ * Modified By: Lars Behrmann (larsbehrmann@behrmann-it.de)
+ * -----
+ * Copyright 2024 <<larsbehrmann@behrmann-it.de>>, www.behrmann-it.de
+ * Licensed under the Apache License, Version 2.0 (the
+ * );
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
@@ -5,8 +28,8 @@ const config: CapacitorConfig = {
   appName: 'blueprint',
   webDir: 'www',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
