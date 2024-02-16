b3b99ec0d8d875eb448e2b8e95c9b87857e6e23c
Lars Behrmann <lars.behrmann@catchium-gmbh.de>
2024-02-16 22:53:20 +0100

    Add language settings

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Storage } from '@ionic/storage';
+import { LanguageService } from 'src/app/shared/services/language.service';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +9,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
+  //public settings: Settings;
+  public currentLanguage: any;
+  public lang: any;
+  public languages: Array<{ locale: string; language: string }>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private translateService: TranslateService,
+    private languageService: LanguageService,
+    //private settingsService: SettingsService,
+    private storage: Storage
+  ) {
+    this.languages = [
+      { locale: 'de', language: 'GERMAN' },
+      { locale: 'en', language: 'ENGLISH' },
+    ];
+    this.storage.get('AppLanguageV1').then((appLanguage) => {
+      if (appLanguage == null) {
+        this.currentLanguage = this.languageService.getBrowserLanguage();
+      } else {
+        this.currentLanguage = appLanguage;
+      }
+      window['appLocale'] = appLanguage;
+    });
   }
 
+  ngOnInit() {}
+
+  ionViewWillEnter() {
+    // this.settingsService
+    //   .findAll()
+    //   .then((data) => {
+    //     console.log('Settings loaded ', data);
+    //     this.settings = data;
+    //   })
+    //   .catch((error) => {
+    //     console.log('Error loading settings! ', error);
+    //     alert('Error loading settings! ' + error);
+    //   });
+  }
+
+  switchLanguage() {
+    console.log('Current language' + this.currentLanguage);
+    if (this.currentLanguage != this.lang) {
+      this.storage.set('AppLanguageV1', this.lang);
+      //this.translateService.setDefaultLang(this.lang);
+      this.translateService.use(this.lang);
+      // let setting = this.settings.get(SettingKeys.LANGUAGE);
+      // setting.val = this.lang;
+      // this.settingsService.update(setting).catch((error) => {
+      //   alert('Unexpedcted error ' + error);
+      // });
+      // setTimeout(() => {
+      //   window.location.reload();
+      // }, 250);
+    }
+  }
 }
