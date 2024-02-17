aa494da9a39f0b9ba882452271ea4e2f5a5bf603
Lars Behrmann <lars.behrmann@catchium-gmbh.de>
2024-02-17 15:50:47 +0100

    Cleanup LanguageService

--- src/app/shared/services/language.service.ts
+++ src/app/shared/services/language.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { Storage } from '@ionic/storage-angular';
-//import { resolve } from 'url';
 
 @Injectable({
   providedIn: 'root',
@@ -16,16 +15,7 @@ export class LanguageService {
     private translateService: TranslateService,
     private storage: Storage
   ) {
-    // console.log('Storage create');
-    // this.storage.create();
     this.init();
-    // translateService.addLangs(['en', 'de']);
-    // translateService.onLangChange.subscribe((event: LangChangeEvent) => {
-    //   console.log("onLangChange", event.translations);
-    // });
-    // translateService.onDefaultLangChange.subscribe((event: LangChangeEvent) => {
-    //   console.log("onDefaultLangChange", event.translations);
-    // });
   }
 
   async init() {
@@ -34,23 +24,6 @@ export class LanguageService {
     console.log('Storage create');
     await this.storage.create();
     this.translateService.addLangs(['en', 'de']);
-  }
-
-  /**
-   * If lang is null. It will try to use
-   * the lang provided by the browser.
-   * Default: en-US
-   */
-  getLocaleByLang(lang: string): string {
-    if (!lang) {
-      lang = 'de-DE'; // getBrowserLanguage();
-    }
-    for (var key in this.currentLocales) {
-      if (this.currentLocales.hasOwnProperty(key)) {
-        return this.currentLocales[key];
-      }
-    }
-    return this.currentLocales.en;
   }
 
   initAppLanguage() {
@@ -77,9 +50,8 @@ export class LanguageService {
   }
 
   getBrowserLanguage(): string {
-    // Language should be set during account
-    // registration by the association.
-    // And later for free accounts by the accept
+    // Language should be set during first
+    // app initialisation
     // like this ajax request https://stackoverflow.com/questions/1043339/javascript-for-detecting-browser-language-preference
     //return 'de';
     console.debug('Lang ', window.navigator.language);
@@ -95,20 +67,5 @@ export class LanguageService {
       return browserLang;
     }
     return 'en';
-  }
-
-  getBrowserLanguageAsPath(): string {
-    // let browserLang = getBrowserLanguage();
-    // if (browserLang && browserLang === 'de') {
-    return 'de/de';
-    //}
-    // let browserLang = window.navigator.language;
-    // if (browserLang && browserLang.indexOf('-') > -1) {
-    //   return browserLang.replace('-', '/').toLowerCase();
-    // }
-    // if (browserLang && browserLang === 'de') {
-    //   return 'de/de';
-    // }
-    //return 'en/us';
   }
 }
